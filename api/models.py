a58716d1e2abd980d4e919c45ae9b3c9a8cbfae0
Pankaj Padekar <padekarpankaj@gmail.com>
2023-10-22 21:58:25 +0530

    working

--- api/models.py
+++ api/models.py
@@ -15,3 +15,12 @@ class Company(models.Model):
     active = models.BooleanField(default=True)
 
 #Employee model
+
+class Employee(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.CharField(max_length=50)
+    address = models.CharField(max_length=200)
+    phone = models.CharField(max_length=15)
+    position = models.CharField(max_length=50 , choices=(('Manager', 'manager'),('Software Developer','sd'),('Project Leader', 'pl')))
+
+    company = models.ForeignKey(Company, on_delete=models.CASCADE)
